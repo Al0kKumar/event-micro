4719fa5a74847ee25f03812b65870771e884abd7
Alok <mishraalok189381@gmail.com>
2025-08-20 15:18:01 +0530

    added payment status and order place dto

---
+++ notification-service/src/main/java/com/alok/notificationservice/dto/OrderPlacedEvent.java
@@ -0,0 +1,15 @@
+package com.alok.notificationservice.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class OrderPlacedEvent {
+    private Long orderId;
+    private Long userId;
+    private String userEmail;
+    private double amount;
+}
